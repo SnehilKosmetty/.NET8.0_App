bf0974af0834f5869a68b69c17da520305131cb7
Snehil <snehildanuel21@gmail.com>
2024-09-30 21:59:19 -0400

    Add project files.

---
+++ Enterprise_Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Enterprise_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
